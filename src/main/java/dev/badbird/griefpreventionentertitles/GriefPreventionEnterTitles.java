fc5e4931111c65dca53ccc79b4b3fa71ebc7b913
pfeiferocks <github@pfeiferocks.de>
2024-02-14 23:02:01 +0100

    switched to gradle changed api to papermc 1.20

--- src/main/java/dev/badbird/griefpreventionentertitles/GriefPreventionEnterTitles.java
+++ src/main/java/dev/badbird/griefpreventionentertitles/GriefPreventionEnterTitles.java
@@ -2,7 +2,7 @@ package dev.badbird.griefpreventionentertitles;
 
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
@@ -10,13 +10,13 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -27,10 +27,11 @@ import java.util.UUID;
 
 public final class GriefPreventionEnterTitles extends JavaPlugin implements Listener {
 
-    private static Map<UUID, Claim> claimMap = new HashMap<>();
+    private static final Map<UUID, Claim> claimMap = new HashMap<>();
 
     private static MiniMessage miniMessage;
 
+    private static boolean debug;
     private static String enterTitle;
     private static String enterSubtitle;
     private static String leaveTitle;
@@ -39,18 +40,8 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
     private static String leaveActionbar;
     private FileConfiguration config;
 
-    private BukkitAudiences adventure;
-
-    public BukkitAudiences adventure() {
-        if(this.adventure == null) {
-            throw new IllegalStateException("Tried to access Adventure when the plugin was disabled!");
-        }
-        return this.adventure;
-    }
-
     @Override
     public void onEnable() {
-        adventure = BukkitAudiences.create(this);
         miniMessage = MiniMessage.miniMessage();
         if (!getDataFolder().exists()) getDataFolder().mkdir();
         if (!new File(getDataFolder() +  "/config.yml").exists()) saveDefaultConfig();
@@ -67,6 +58,7 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
             leaveSubtitle = getConfig().getString(leaveBase + ".subtitle");
             leaveActionbar = getConfig().getString(leaveBase + ".actionbar");
         }
+        debug = getConfig().getBoolean("debug",false);
     }
 
     @Override
@@ -84,7 +76,7 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        onMove(event.getPlayer(), event.getFrom(), event.getTo());
+        onMove(event.getPlayer(), event.getPlayer().getUniqueId(), event.getFrom(), event.getTo());
     }
 
     @EventHandler
@@ -97,7 +89,7 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
         claimMap.remove(event.getPlayer().getUniqueId());
     }
 
-    private void onMove(Player player, Location from, Location to) {
+    private void onMove(final @NonNull Audience player, UUID uuid, Location from, Location to) {
         World fromWorld = from.getWorld();
         World toWorld = to.getWorld();
 
@@ -108,11 +100,11 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
             if (!worlds.contains(toWorld.getName())) return;
         }
 
-        Claim cachedClaim = claimMap.get(player.getUniqueId());
+        Claim cachedClaim = claimMap.get(uuid);
         Claim movingTo = GriefPrevention.instance.dataStore.getClaimAt(from, true, cachedClaim);
         if (cachedClaim == null && movingTo != null) { //Entering a claim
-            //System.out.println("Entering a claim");
-            Component enter = null, sub = null, action = null;
+            if (debug) System.out.println("Entering a claim");
+            Component enter, sub;
             if (enterTitle != null && !enterTitle.isEmpty()) {
                 enter = miniMessage.deserialize(enterTitle.replace("%player%", movingTo.getOwnerName()));
             } else enter = Component.empty();
@@ -120,33 +112,33 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
                 sub = miniMessage.deserialize(enterSubtitle.replace("%player%", movingTo.getOwnerName()));
             } else sub = Component.empty();
 
-            claimMap.put(player.getUniqueId(), movingTo);
+            claimMap.put(uuid, movingTo);
             Title title = Title.title(enter, sub);
             // player.showTitle(title);
-            adventure().player(player).showTitle(title);
+            player.showTitle(title);
 
             if (enterActionbar != null && !enterActionbar.isEmpty()) {
                 // player.sendActionBar(miniMessage.deserialize(enterActionbar.replace("%player%", movingTo.getOwnerName())));
-                adventure().player(player).sendActionBar(miniMessage.deserialize(enterActionbar.replace("%player%", movingTo.getOwnerName())));
+                player.sendActionBar(miniMessage.deserialize(enterActionbar.replace("%player%", movingTo.getOwnerName())));
             }
         } else if (cachedClaim != null && movingTo == null) { //Leaving a claim
-            //System.out.println("Leaving a claim");
-            Component leave = null, sub = null;
+            if (debug) System.out.println("Leaving a claim");
+            Component leave, sub;
             if (leaveTitle != null && !leaveTitle.isEmpty()) {
                 leave = miniMessage.deserialize(leaveTitle.replace("%player%", cachedClaim.getOwnerName()));
             } else leave = Component.empty();
             if (leaveSubtitle != null && !leaveSubtitle.isEmpty()) {
                 sub = miniMessage.deserialize(leaveSubtitle.replace("%player%", cachedClaim.getOwnerName()));
             } else sub = Component.empty();
-            claimMap.remove(player.getUniqueId());
-            //System.out.println("Title: " + leave + " | " + sub);
+            claimMap.remove(uuid);
+            if (debug) System.out.println("Title: " + leave + " | " + sub);
             Title title = Title.title(leave, sub);
             // player.showTitle(title);
-            adventure().player(player).showTitle(title);
+            player.showTitle(title);
 
             if (leaveActionbar != null && !leaveActionbar.isEmpty()) {
                 // player.sendActionBar(miniMessage.deserialize(leaveActionbar.replace("%player%", cachedClaim.getOwnerName())));
-                adventure().player(player).sendActionBar(miniMessage.deserialize(leaveActionbar.replace("%player%", cachedClaim.getOwnerName())));
+                player.sendActionBar(miniMessage.deserialize(leaveActionbar.replace("%player%", cachedClaim.getOwnerName())));
             }
         }
     }
